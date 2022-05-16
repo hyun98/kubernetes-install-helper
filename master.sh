9cfdac91a88129719b22a1843188892b55a2cb49
hyun0404woo <hyun0404woo@gmail.com>
2022-05-17 00:10:52 +0900

    rm prev k8stoken

--- master.sh
+++ master.sh
@@ -3,6 +3,8 @@
 # init K8s
 sudo kubeadm init --token-ttl 0 --pod-network-cidr=172.16.0.0/16 >> kubeadm-init-result.txt
 
+sudo rm k8s-token.txt
+
 # token saved at k8s-token.txt
 # This token has no expiry date
 cat kubeadm-init-result.txt | tail -2 >> k8s-token.txt
