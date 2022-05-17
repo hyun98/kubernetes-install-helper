59229c08ea5245449aebb609d086678a4748e240
hyun0404woo <hyun0404woo@gmail.com>
2022-05-17 22:02:29 +0900

    calico network

--- master.sh
+++ master.sh
@@ -21,6 +21,3 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 # config Calico network
 kubectl create -f https://projectcalico.docs.tigera.io/manifests/tigera-operator.yaml
-
-kubectl create -f https://projectcalico.docs.tigera.io/manifests/custom-resources.yaml
-
