e0ba38d42da5a5836c4ff963e18f84866ffecf62
hyun0404woo <hyun0404woo@gmail.com>
2022-09-27 16:07:08 +0900

    오타수정

--- k3s/k3s-worker.sh
+++ k3s/k3s-worker.sh
@@ -8,7 +8,7 @@ fi
 
 WORKER_NUM=$1
 
-curl -sfL https://get.k3s.io | K3SURL=https://$MASTER_IP:6443 \
+curl -sfL https://get.k3s.io | K3S_URL=https://$MASTER_IP:6443 \
 K3S_TOKEN=$NODE_TOKEN \
 INSTALL_K3S_EXEC="--node-name aomd-worker${WORKER_NUM} --docker" \
 INSTALL_K3S_VERSION="v1.23.6+k3s1" sh -s -
