582303d183fa257bc5d276278838f1791a3179ac
hyun0404woo <hyun0404woo@gmail.com>
2022-09-27 15:54:32 +0900

    version 수정 1.18.6 -> 1.23.6

--- k3s/k3s-master.sh
+++ k3s/k3s-master.sh
@@ -6,11 +6,11 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="\
 --disable traefik \
 --disable metrics-server \
 --node-name aomd-master --docker" \
-INSTALL_K3S_VERSION="v1.18.6+k3s1" sh -s -
+INSTALL_K3S_VERSION="v1.23.6+k3s1" sh -s -
 
 mkdir ~/.kube
 sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
 sudo chown -R $(id -u):$(id -g) ~/.kube
 echo "export KUBECONFIG=~/.kube/config" >> ~/.bashrc
-echo "alias k='kubectl'" >> ~/.bashrc
+echo "alias k=\"kubectl\"" >> ~/.bashrc
 source ~/.bashrc
